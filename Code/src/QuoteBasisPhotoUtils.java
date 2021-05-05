dfa7f6484911fb742ed336f2da0721b9dfbd562c
Spike81 <52449420+Spike81@users.noreply.github.com>
2021-05-05 16:26:59 +0800

    commit

--- Code/src/QuoteBasisPhotoUtils.java
+++ Code/src/QuoteBasisPhotoUtils.java
@@ -9,7 +9,9 @@ import java.awt.event.MouseWheelListener;
 import javax.swing.*;
 
 public class QuoteBasisPhotoUtils extends JFrame {
-
+    /**
+     *
+     */
     private static final long serialVersionUID = 1L;
     private ZoomPanel zp;
 
@@ -26,15 +28,20 @@ public class QuoteBasisPhotoUtils extends JFrame {
         setSize(frameSize);
         setLocationRelativeTo(null);// 居中显示
 
+        // JScrollPane scrollPane_1 = new JScrollPane();
+        // getContentPane().add(scrollPane_1, BorderLayout.CENTER);
+        // scrollPane_1
+        // .setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-        zp = new ZoomPanel(frameSize, file);
-        getContentPane().add(zp);
+//        zp = new ZoomPanel(frameSize, file);
+//        getContentPane().add(zp);
 
+        // scrollPane_1.setViewportView(zp);
 
         // 添加鼠标滚轮消息响应
         addMouseWheelListener(new MouseWheelListener() {
             public void mouseWheelMoved(MouseWheelEvent e) {
-
+                System.out.println("sadfdasdfsa");
                 // 滚轮向前滑动 放大
                 if (e.getWheelRotation() < 0) {
                     zp.enlargeImg();
@@ -47,84 +54,82 @@ public class QuoteBasisPhotoUtils extends JFrame {
             }
         });
 
+
         setResizable(false);
         setVisible(true);
     }
 
+    public static void main(String[] args) {
+        new QuoteBasisPhotoUtils("img/background.jpg");
+    }
 }
 
 // 缩放面板
-class ZoomPanel extends JPanel {
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
-    private ImageIcon ii;
-    private Dimension frameSize;
-    private int x, y, width, height;// 图片的坐标、缩放的size
-
-    public ZoomPanel(Dimension fs, String imgPath) {
-        setBackground(Color.WHITE);
-        setToolTipText("滑动滚轮可缩小放大图片");
-        this.frameSize = fs;
-
-        ii = new ImageIcon(imgPath);
-        JPanel a = new JPanel();
-        JLabel label = new JLabel();
-        label.setIcon(ii);
-        a.add(label);
-
-        width = 500;
-        height = 707;
-        setImgPos();
-
-
-
-    }
-
-    // 设定x、y坐标
-    public void setImgPos() {
-        x = (frameSize.width - width) / 2;
-        y = (frameSize.height - height) / 2;
-    }
-
-    // 放大
-    public void enlargeImg() {
-        if (width >= ii.getIconWidth() * 2) {
-            return;
-        }
-        if (height >= ii.getIconHeight() * 2) {
-            return;
-        }
-
-        width += width / 9; // 这里为什么是除以9呢 这个是数学问题 留给大家自己想吧 要是不明白,可以留言
-        height += height / 9;
-        setImgPos();
-
-        repaint();
-    }
-
-    // 缩小
-    public void reduceImg() {
-        if (width <= ii.getIconWidth() / 5) {
-            return;
-        }
-        if (height <= ii.getIconHeight() / 5) {
-            return;
-        }
-
-        width -= width / 10;
-        height -= height / 10;
-        setImgPos();
-
-        repaint();
-    }
-
-    // 重绘
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        g.drawImage(ii.getImage(), x, y, width, height, null);
-    }
-
-}
+//class ZoomPanel extends JLabel {
+//    /**
+//     *
+//     */
+//    private static final long serialVersionUID = 1L;
+//    private ImageIcon ii;
+//    private Dimension frameSize;
+//    private int x, y, width, height;// 图片的坐标、缩放的size
+//
+//    public ZoomPanel(Dimension fs, String imgPath) {
+//        setBackground(Color.WHITE);
+//        setToolTipText("滑动滚轮可缩小放大图片");
+//        this.frameSize = fs;
+//
+//        ii = new ImageIcon(imgPath);
+//        JLabel label = new JLabel();
+//        label.setIcon(ii);
+//
+//        width = 500;
+//        height = 707;
+//        setImgPos();
+//    }
+//
+//    // 设定x、y坐标
+//    public void setImgPos() {
+//        x = (frameSize.width - width) / 2;
+//        y = (frameSize.height - height) / 2;
+//    }
+//
+//    // 放大
+//    public void enlargeImg() {
+//        if (width >= ii.getIconWidth() * 2) {
+//            return;
+//        }
+//        if (height >= ii.getIconHeight() * 2) {
+//            return;
+//        }
+//
+//        width += width / 9; // 这里为什么是除以9呢 这个是数学问题 留给大家自己想吧 要是不明白,可以留言
+//        height += height / 9;
+//        setImgPos();
+//
+//        repaint();
+//    }
+//
+//    // 缩小
+//    public void reduceImg() {
+//        if (width <= ii.getIconWidth() / 5) {
+//            return;
+//        }
+//        if (height <= ii.getIconHeight() / 5) {
+//            return;
+//        }
+//
+//        width -= width / 10;
+//        height -= height / 10;
+//        setImgPos();
+//
+//        repaint();
+//    }
+//
+//    // 重绘
+//    public void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//
+//        g.drawImage(ii.getImage(), x, y, width, height, null);
+//    }
+//}
--- Code/src/hotel_booker.java
+++ Code/src/hotel_booker.java
@@ -3,6 +3,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Font;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 public class hotel_booker {
 
@@ -103,8 +105,30 @@ public class hotel_booker {
             }
         });
 
+//        bookPanel.addMouseWheelListener(new MouseWheelListener() {
+//            public void mouseWheelMoved(MouseWheelEvent e) {
+//
+//                Dimension frameSize = Toolkit.getDefaultToolkit().getScreenSize();
+//                frameSize.width = frameSize.width / 2;
+//                frameSize.height = frameSize.height * 3 / 4;
+//                String file = "img/map.jpg";
+//
+//                ZoomPanel zp = new ZoomPanel(frameSize, file, bookPanel);
+////                 滚轮向前滑动 放大
+//                if (e.getWheelRotation() < 0) {
+//                    zp.enlargeImg();
+//                    System.out.println("aaaaa");
+//                }
+//
+//                // 滚轮向后滑动 缩小
+//                else {
+//                    zp.reduceImg();
+//                    System.out.println("sssss");
+//                }
+//            }
+//        });
 
-        frame.getLayeredPane().add(label, new Integer(Integer.MIN_VALUE));
+        frame.getLayeredPane().add(label, Integer.valueOf(Integer.MIN_VALUE));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         int width = Toolkit.getDefaultToolkit().getScreenSize().width;
@@ -193,16 +217,34 @@ public class hotel_booker {
     private static void book(JPanel panel) {
         panel.setLayout(null);
 
-        JPanel mapPanel = new JPanel();
-        mapPanel.setBounds(30,100,240,300);
-        panel.add(mapPanel);
+        Dimension frameSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frameSize.width = frameSize.width / 2;
+        frameSize.height = frameSize.height / 4;
+        String file = "img/map.jpg";
 
-        QuoteBasisPhotoUtils photo = new QuoteBasisPhotoUtils("img/background.jpg");
+        ZoomPanel zp = new ZoomPanel(frameSize, file);
+        zp.setBounds(30,100,240,300);
+        panel.add(zp);
+
+        panel.addMouseWheelListener(new MouseWheelListener() {
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                // 滚轮向前滑动 放大
+                if (e.getWheelRotation() < 0) {
+                    zp.enlargeImg();
+                    System.out.println("aaaaa");
+                }
+                // 滚轮向后滑动 缩小
+                else {
+                    zp.reduceImg();
+                    System.out.println("sssss");
+                }
+            }
+        });
 
         MyComponent component=new MyComponent();
         component.setBounds(60,40,470,20);
         component.setArrText(new String []{"Welcome to using Hotel Book System!",
-                                            "The COVID-19 epidemic is raging, please wear masks when entering and exiting, pay attention to safe social distancing, and strengthen protective measures"
+                                           "The COVID-19 epidemic is raging, please wear masks when entering and exiting, pay attention to safe social distancing, and strengthen protective measures"
                                             });
         panel.add(component);
 
@@ -217,13 +259,80 @@ public class hotel_booker {
 
         bookresetButton.setBounds(460, 375, 80, 25);
         panel.add(bookresetButton);
-
     }
 
     private static void user(JPanel panel) {
         panel.setLayout(null);
 
 
+    }
+
+}
+
+class ZoomPanel extends JPanel {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private ImageIcon ii;
+    private Dimension frameSize;
+    private int x, y, width, height;// 图片的坐标、缩放的size
+
+    public ZoomPanel(Dimension fs, String imgPath) {
+        setToolTipText("Slide scroll of mouse wheel to zoom in and out");
+        this.frameSize = fs;
+
+        ii = new ImageIcon(imgPath);
+
+        width = 240;
+        height = 300;
+        setImgPos();
+
+    }
+
+    // 设定x、y坐标
+    public void setImgPos() {
+        x = (frameSize.width - width) / 2 - 263;
+        y = (frameSize.height - height) / 2 + 43;
+    }
+
+    // 放大
+    public void enlargeImg() {
+        if (width >= ii.getIconWidth() * 2) {
+            return;
+        }
+        if (height >= ii.getIconHeight() * 2) {
+            return;
+        }
+
+        width += width / 9;
+        height += height / 9;
+        setImgPos();
+
+        repaint();
+    }
+
+    // 缩小
+    public void reduceImg() {
+        if (width <= ii.getIconWidth() / 4) {
+            return;
+        }
+        if (height <= ii.getIconHeight() / 4) {
+            return;
+        }
+
+        width -= width / 10;
+        height -= height / 10;
+        setImgPos();
+
+        repaint();
+    }
+
+    // 重绘
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.drawImage(ii.getImage(), x, y, width, height, null);
     }
 
 }
